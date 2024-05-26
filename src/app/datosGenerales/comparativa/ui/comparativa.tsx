84abf336e52d66abae003533ac676383753d4187
Juan Carlos Condori Machicado <juanc.com@gmail.com>
2024-05-26 14:03:42 -0400

    reporte pdf ajustes

--- src/app/datosGenerales/comparativa/ui/comparativa.tsx
+++ src/app/datosGenerales/comparativa/ui/comparativa.tsx
@@ -309,7 +309,7 @@ const ComparativaComponent = ({ infoSectorData }: InformacionInterface) => {
 
                 return (
                   <Grid item xs={12} sm={6} key={index} sx={{ height: '50%' }}>
-                    <Item
+                    <Paper
                       ref={activePaperRefs.current[itemName][index % 2]}
                       elevation={4}
                       sx={{ height: '100%' }}
@@ -330,7 +330,7 @@ const ComparativaComponent = ({ infoSectorData }: InformacionInterface) => {
                           Gráfico Placeholder {index + 1}
                         </Typography>
                       )}
-                    </Item>
+                    </Paper>
                   </Grid>
                 )
               } else {
--- src/app/datosGenerales/types/datosGeneralesType.tsx
+++ src/app/datosGenerales/types/datosGeneralesType.tsx
@@ -19,6 +19,7 @@ export interface Variable {
 export interface Graficos {
   id: string
   titulo: string
+  ancho: string
   tipoGrafico: TipoGrafico
 }
 
