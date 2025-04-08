78a89132d268cab254d27b0817f0373454026cad
Gustavo Poma <gustavo.poma.quispe@gmail.com>
2025-04-08 16:49:51 -0400

    fix: eliminacion de modulos

--- src/app/admin/(configuracion)/modulos/types/ModalModuloType.ts
+++
@@ -1,8 +0,0 @@
-import { ModuloCRUDType } from '@/app/admin/(configuracion)/modulos/types/CrearEditarModulosType'
-
-export interface ModalModuloType {
-  modulo?: ModuloCRUDType | undefined | null
-  accionCorrecta: () => void
-  accionCancelar: () => void
-  modulos: ModuloCRUDType[]
-}
--- src/app/admin/(configuracion)/roles/types/rolCRUDType.ts
+++
@@ -1,7 +0,0 @@
-export interface RolCRUDType {
-  id?: string
-  nombre: string
-  descripcion: string
-  rol: string
-  estado: string
-}
